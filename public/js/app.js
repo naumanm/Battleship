2b43e4c08f89bb461b339097db6f5648f9cfb56b
michael nauman <bodhiandmike@gmail.com>
2015-03-25 13:40:15 -0700

    wrapped mouse effects in a gaemPlay function

--- public/js/app.js
+++ public/js/app.js
@@ -1,44 +1,48 @@
 $(document).ready(function() {
 
-  // globals
-  var selectedArr = []; // array of all shots
 
-  // color change on hover
-  $("td").mouseover(function(){
-    var cellState = $(this).data("state");
-    var cellId = $(this).data("id");
-    var cellTable = $(this).closest("table").attr("class");
-    if (cellId !== "header" && cellState === "unselected" && cellTable === "opponent") {
-      $(this).css("background-color", "red");
-    }
-  });
+  function gamePlay() {
 
-  // revert color if not clicked
-  $("td").mouseleave(function(){
-    var cellState = $(this).data("state");
-    if (cellState === "unselected") {
-      $(this).css("background-color", "gray");  // if not selected change color back
-    }
-  });
+    var selectedArr = []; // array of all shots
 
-  // select to take a shot
-  $("td").click(function(){
-    var cellId = $(this).data("id"); // get the cellId for the current cell
-    var cellState = $(this).data("state");
-    var cellTable = $(this).closest("table").attr("class");
-    if (cellId !== 'header' && cellState === "unselected" && cellTable === "opponent") {
-      $(this).css("background-color", "blue");
-      $(this).data("state", "miss");
-      if (selectedArr.indexOf(cellId) === -1) { // prevent duplicates in the selectedArr
-        selectedArr.push(cellId); // push the selected cell into the selectedArr
-        console.log(selectedArr);
+    // color change on hover
+    $("td").mouseover(function(){
+      var cellState = $(this).data("state");
+      var cellId = $(this).data("id");
+      var cellTable = $(this).closest("table").attr("class");
+      if (cellId !== "header" && cellState === "unselected" && cellTable === "opponent") {
+        $(this).css("background-color", "red");
       }
-    }
-  });
+    });
+
+    // revert color if not clicked
+    $("td").mouseleave(function(){
+      var cellState = $(this).data("state");
+      if (cellState === "unselected") {
+        $(this).css("background-color", "gray");  // if not selected change color back
+      }
+    });
+
+    // select to take a shot
+    $("td").click(function(){
+      var cellId = $(this).data("id"); // get the cellId for the current cell
+      var cellState = $(this).data("state");
+      var cellTable = $(this).closest("table").attr("class");
+      if (cellId !== 'header' && cellState === "unselected" && cellTable === "opponent") {
+        $(this).css("background-color", "blue");
+        $(this).data("state", "miss");
+        if (selectedArr.indexOf(cellId) === -1) { // prevent duplicates in the selectedArr
+          selectedArr.push(cellId); // push the selected cell into the selectedArr
+          console.log(selectedArr);
+        }
+      }
+    });
+  }  
 
 
   function initialize() {
     console.log("board is loaded");
+    gamePlay();
   }
 
   initialize();
