b285341e417a94d5f16649ed711b4e7492eb27e0
Christian Chandler <247webdev+github@googlemail.com>
2015-03-27 08:47:35 -0700

    christian's initial commit. no serious changes, just pseudo code in comments so far.

---
+++ app.js
@@ -0,0 +1,83 @@
+// entry point when server starts
+
+// setup env
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    redis = require("redis"),
+    client = redis.createClient(),
+    methodOverride = require("method-override"),
+    bodyParser = require("body-parser");
+
+
+// allows us to use ejs instead of html
+app.set("view engine", "ejs");
+ 
+// location for static files
+app.use(express.static(__dirname + '/public'));
+
+// root route
+app.get('/', function(req, res){
+  res.render("index.ejs");
+});
+
+// game communication
+io.on('connection', function(socket){
+  socket.join("Game");
+
+  console.log('a user connected');
+
+  socket.on('shot', function(shotObj){
+    io.emit('shot', shotObj);
+    client.LPUSH("shotFired", shotObj);
+    console.log(shotObj);
+  });
+
+  socket.on('playerJoined', function(player) {
+    console.log(player);
+    // push player to redis
+    client.LPUSH("playerList", player);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+// loop to check for players to start game
+// need to replace this with a form and be 
+// user driven
+setInterval(function(){checkTwoPlayers()}, 5000);
+
+
+function checkTwoPlayers() {
+  console.log("Start Game check");
+
+  client.lrange('playerList', 0, -1, function(err, reply) {
+      console.log(reply);
+      if (reply.length === 2) {
+        startGame(reply)
+      };
+  });
+}
+
+
+function startGame(playerList){
+  console.log("game started");
+
+}
+
+// when get to the point when there is a winner
+// use this function on each move to check if either player lost all of their ships
+//pseudo code
+//function (--==somePlayerObject==--) {
+//  boatArray.each
+//}
+
+
+// load our server
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ public/js/app.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+  function gamePlay() {
+
+    var socket = io();
+    var selectedArr = []; // array of all shots
+
+    // color change on hover
+    $("td").mouseover(function(){
+      var cellState = $(this).data("state");
+      var cellId = $(this).data("id");
+      var cellTable = $(this).closest("table").attr("class");
+      if (cellId !== "header" && cellState === "unselected" && cellTable === "opponent") {
+        $(this).css("background-color", "red");
+      }
+    });
+
+    // revert color if not clicked
+    $("td").mouseleave(function(){
+      var cellState = $(this).data("state");
+      if (cellState === "unselected") {
+        $(this).css("background-color", "gray");  // if not selected change color back
+      }
+    });
+
+
+    // NEED logic in here to prevent the wrong person from shooting
+    // also need logic to prompt who's turn it is.  
+
+
+
+
+    // select to take a shot
+    $("td").click(function(){
+      var cellId = $(this).data("id"); // get the cellId for the current cell
+      var cellState = $(this).data("state");
+      var cellTable = $(this).closest("table").attr("class");
+      if (cellId !== 'header' && cellState === "unselected" && cellTable === "opponent") {
+        $(this).css("background-color", "blue");
+        $(this).data("state", "miss");
+        if (selectedArr.indexOf(cellId) === -1) { // prevent duplicates in the selectedArr
+          selectedArr.push(cellId); // push the selected cell into the selectedArr
+          var shotObj = {};
+          shotObj.player = person;
+          shotObj.id = cellId;
+          socket.emit('shot', shotObj);
+        }
+      }
+    });
+  }  
+
+  function initialize() {
+    //clear REDIS tables
+    //initialize all variables
+    //establish new boat arrays
+    gamePlay();
+  }
+
+  initialize();
+
+});
