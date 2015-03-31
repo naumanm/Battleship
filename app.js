4cd8deefc5de7e09cb7068edd4f1ba2e38005af3
Will Simmons <willsimmons@gmail.com>
2015-03-31 10:57:34 -0700

    game lobby and matchmaking working, testing for game playability with seed data

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ var express = require('express'),
     playerPair=0,
     bodyParser = require("body-parser"),
     waitingRoom =[], 
-    gameRooms=[], 
-    drydock=[]; 
+    gameRooms=[]; 
 // allows us to use ejs instead of html
 app.set("view engine", "ejs");
 
@@ -50,27 +49,27 @@ io.on('connection', function(socket){  //step #1 connection
   socket.join(roomNumber);
   console.log(roomNumber);
   
-  console.log('a user connected');
+  console.log(socket.id + " connected");
 
-  socket.on('playerName', function(playerName) {  //step # building a lobby
-    socket.nickname=playerName;
-    client.HSETNX("playersName", socket.id, socket.id);  //this is the socket has not the actual user name
+  //socket.on('playerName', function(playerName) {  //step # building a lobby
+    socket.nickname=socket.id; 
+    console.log(socket.id);//change to player name once modal is working correctly
+    //client.HSETNX("playersName", socket.id, socket.id);  //this is the socket has not the actual user name
     //unsure of the use of this, as this is maintained by the game object and socket io room, which is temporary
-    client.HSETNX("gameIDs", socket.id, socket.id);  //connecting the first player as a game id ref point
-    waitingRoom.push(Player.new()); //need to save in session as value with nickname as key for reconnect?
+    //client.HSETNX("gameIDs", socket.id, socket.id);  //connecting the first player as a game id ref point
+    waitingRoom.push(socket.id); //need to save in session as value with nickname as key for reconnect?
     playerPair++;
     //assign a game, roomNumber, and reset queue when two players are in the waiting room
     if (playerPair===2){
-      client.HSETNX("opponent", socket.id, socket.id);
+      //client.HSETNX("opponent", socket.id, socket.id);
       // in case line above doesn't work client.HSETNX("opponent", gameObj.playerID, gameObj.opponentID); 
-      gameRooms.push(Game.new(waitingRoom[0],waitingRoom[1],roomNumber));
-     // gameRooms[length-1].runGame(); //starts the game DO NOT DELETE, CAN'T ACTIVATE YET
+      gameRooms.push(new Game(waitingRoom[0],waitingRoom[1],roomNumber));
       waitingRoom=[];
       roomNumber++;
       playerPair=0;
     }
 
-  });
+  //});
   
   //ORIGINAL SHOT FUNCTION KEEP THIS 
   // socket.on('shot', function(shotObj){  //#step 3 firing a shot in the game
@@ -80,8 +79,8 @@ io.on('connection', function(socket){  //step #1 connection
   // });
 
   socket.on('disconnect', function(){
-    console.log(socket.nickname + " disconnected");
-    if (!socket.nickname) return;              
+    console.log(socket.id + " disconnected");
+    //if (!socket.nickname) return;              
   });
   
 });
@@ -89,24 +88,32 @@ io.on('connection', function(socket){  //step #1 connection
 //game logic step 2(A) building the board
 function Game (player1,player2,gameId){  
   this.player1=player1;
+  console.log(player1 + "player 1");
   this.player2=player2;
+  console.log(player2 +"player2");
   this.gameId=gameId;  //gameroom
+  console.log(gameId + "game id");
   gameOver=false;
-  player1.on('fleetReady',function(data){
-     carrier=[]; //get this from client end
-     battle=[];
-     sub=[];
-     pt =[];
-     var player1Fleet = newFleet(carrier,battle,sub,pt); 
-   });
-  player2.on('fleetReady',function(data){
-     carrier=[]; //get this from client end
-     battle=[];
-     sub=[];
-     pt =[];
-     var player2Fleet = newFleet(carrier,battle,sub,pt); 
-   }); 
+  //dummy data
+  var player1Fleet=new Fleet(player1,["a1","a2","a3","a4","a5"],["b1","b2","b3","b4"],["c1","c2","c3"],["d1","d2"]);
+  var player2Fleet=new Fleet(player2,["a1","a2","a3","a4","a5"],["b1","b2","b3","b4"],["c1","c2","c3"],["d1","d2"]);
+  //keep this for live play, seeding dummy test fire data
+  // player1.on('fleetReady',function(data){
+  //    carrier=[]; //get this from client end hard code test
+  //    battle=[];
+  //    sub=[];
+  //    pt =[];
+  //    var player1Fleet = newFleet(carrier,battle,sub,pt); 
+  //  });
+  // player2.on('fleetReady',function(data){
+  //    carrier=[]; //get this from client end hard code test
+  //    battle=[];
+  //    sub=[];
+  //    pt =[];
+  //    var player2Fleet = newFleet(carrier,battle,sub,pt); 
+  //  }); 
   turnController=1;
+  console.log("move# " + turnController);
   if (turnController%2===0)
    {
     player1.on('shot', function(shotObj){  //#step 3 firing a shot in the game
@@ -114,6 +121,7 @@ function Game (player1,player2,gameId){
       //need to add flash event for player click while not their turn
       console.log("player 1 shot"+shotObj);
       turnController++;
+      console.log("move# "+ turnController);
     });
     this.hitOrMiss(shotObj,this.player2Fleet);
    }
@@ -123,7 +131,8 @@ function Game (player1,player2,gameId){
      io.emit('shot', shotObj); 
      //need to add flash event for player click while not their turn
      console.log("player 1 shot"+shotObj);
-      turnController++;
+     turnController++;
+     console.log("move# "+ turnController);
     });
      this.hitOrMiss(shotObj,this.player1Fleet);
    }
@@ -146,14 +155,7 @@ Game.prototype.hitOrMiss = function(shotObj,targetPlayerFleet) {
         }
      }
    });
-}
-      
-//game controller
-Game.prototype.runGame = function(){
-  while (this.gameOver===false){
-    this.hitOrMiss();
-  }
-}
+};
 
 function Fleet (owner,carrier,battleship,submarine,ptboat){
   this.owner=owner; //socket who placed ship
