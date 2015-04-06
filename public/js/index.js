d706871c8f4eb80d0f5bd3c3a895554c36a47081
michael nauman <bodhiandmike@gmail.com>
2015-04-06 15:15:42 -0700

    UI is now displaying hits or misses depending on shooter

--- public/js/index.js
+++ public/js/index.js
@@ -139,12 +139,22 @@ var socket = io(),
 
       console.log("shot player: " + shotObj.player);
       console.log("game object player: " + gameObj.playerName);
+      console.log(shotObj);
 
       // this is where the client side shooting control will go
 
-      document.getElementById("shotPlayer").innerHTML = shotObj.player + " took a shot at " + shotObj.id;
+
+      if (shotObj.hitORmiss){
+        document.getElementById("shotPlayer").innerHTML = shotObj.player + " HIT at " + shotObj.id;
+      }
+      else {
+        document.getElementById("shotPlayer").innerHTML = shotObj.player + " missed at " + shotObj.id;        
+      }
+
+
 
       if (shotObj.player !== gameObj.playerName) {
+        // this is the current shooter
 
         document.getElementById("userName").innerHTML =  "FIRE " + gameObj.playerName + "!";
         // gets the shot fired and updates the gameboard
@@ -157,19 +167,21 @@ var socket = io(),
         // hitArr[2] = opponent's grid hit DON'T USE HERE
         // hitArr[3] = opponent's grid miss DON'T USE HERE
         // $(hitArr[0]).css("background-color", "red"); // data-id=d4
-        if( shotObj.hitORmiss ){ // change the conditional based on what Will sends but the first block is the hit scenario
-          console.log("HIT");
+
+        if( shotObj.hitORmiss ){
           $(hitArr[0]).removeClass("hide"); // the hit img
         } else { // this block is the miss scenario
-          console.log("MISS");
           $(hitArr[0]).removeClass("hide"); // the miss img
         }
-      } else {
+      } 
+  
 
-          document.getElementById("userName").innerHTML =  "Not " + gameObj.playerName + "'s turn";
+      if (shotObj.player === gameObj.playerName) {
+        // this is NOT the current shooter
+        document.getElementById("userName").innerHTML =  "Not " + gameObj.playerName + "'s turn";
 
       }
-  
+
     }); // END of socket.on 'shot'
 
 
